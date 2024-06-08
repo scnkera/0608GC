7427dc68d029600f06d7a8f789fea65cb89c7a51
scnkera <149810869+scnkera@users.noreply.github.com>
2024-06-08 13:22:36 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+// // console.log("testing")
+
+// let firstObj = {
+//     firstName: "Tim",
+//     lastName: "Garcia",
+//     isInstructor: true
+// };
+
+// console.log(firstObj)
+
+// // let firstObj = {
+// //     firstName: "Tim"
+// //     lastName: "Garcia",
+// //     isInstructor: true
+// // };
+
+
+// let obj = {};
+// let person = "Tom";
+
+// console.log(obj)
+
+// obj[person] = "This is a person";
+// console.log(obj)
+
+// obj[1+1+1] = "Three";
+// // obj;
+// console.log(obj)
+
+let obj = {
+    firstName: "Christelle",
+    lastName: "Nkera",
+    occupation: "Data Analyst"
+};
+
+// console.log(obj.firstName);
+// console.log(obj.lastName);
+// console.log(obj.occupation);
+
+// console.log(obj['firstName']);
+// console.log(obj['lastName']);
+// console.log(obj['occupation']);
+
+delete obj.occupation;
+// console.log(obj);
+
+// const hobby = "hobby";
+obj['hobby'] = "listening to music";
+
+console.log(obj)
+
+
+
+
